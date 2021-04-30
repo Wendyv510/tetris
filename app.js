c7c3510fe3e67c7534cc76f08ab2a62527d082b8
Wendy Harris <wendyv510@hotmail.com>
2021-04-30 14:38:31 -0400

    cells

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10 
+
+    console.log(squares)
 })
