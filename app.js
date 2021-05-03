ee57452f731b1d443f8d12a29655d4f3bbafec5a
Wendy Harris <wendyv510@hotmail.com>
2021-05-03 14:59:00 -0400

    ltetromino

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button') 
     const width = 10 
 
-    console.log(squares)
+    const lTetromino = [
+        [1, width+1, width2+1, 2], 
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
 })
