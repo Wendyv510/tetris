cad7c1c48144c9b4320a756c8c90a7150c8c006c
Wendy Harris <wendyv510@hotmail.com>
2021-05-04 15:10:52 -0400

    freeze function

--- app.js
+++ app.js
@@ -59,5 +59,15 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition += width 
         draw()
     }
+
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken')) 
+            random = math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4 
+            draw() 
+        }
+    }
 })
 
