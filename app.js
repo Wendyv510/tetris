c4e2d6739d0712669a865d16d1396dbfe81e8802
Wendy Harris <wendyv510@hotmail.com>
2021-05-13 17:05:58 -0400

    game over

--- app.js
+++ app.js
@@ -222,6 +222,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end' 
+            clearInterval(timerId)
+        }
+    }
 })
 
