c7095b76db4232ebda95e948ed996a5e3445ff10
Wendy Harris <wendyv510@hotmail.com>
2021-05-13 16:50:27 -0400

    check rotated position

--- app.js
+++ app.js
@@ -59,12 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
+            squares[currentPosition + index].getElementsByClassName.backgroundColor = colors[random]
         })
     }
     
     function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino') 
+            squares[currentPosition + index].style.backgroundColor = ''
         })
     }
 
@@ -100,6 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4 
             draw()
             displayShape() 
+            addScore()
+            gameOver() 
         }
     }
 
@@ -126,6 +130,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         draw() 
     }
+
+
+    function isAtRight() {
+        return current.some(index=> (currentPosition + index + 1) % width === 0)  
+      }
+      
+      function isAtLeft() {
+        return current.some(index=> (currentPosition + index) % width === 0)
+      }
+
+      function checkRotatedPosition(P){
+        P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
+        if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
+          if (isAtRight()){            //use actual position to check if it's flipped over to right side
+            currentPosition += 1    //if so, add one to wrap it back around
+            checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+            }
+        }
+        else if (P % width > 5) {
+          if (isAtLeft()){
+            currentPosition -= 1
+          checkRotatedPosition(P)
+          }
+        }
+      }
 
     function rotate(){
         undraw() 
